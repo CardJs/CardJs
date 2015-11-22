196ec4f3891651ed2ab06f6f36cc5450586eabad
Colin Stannard <colin.stannard@cbsindustries.com>
2015-11-22 05:47:47 +0000

    Restructure into separate javascript class with jquery wrapper. Enforeces encapsulation and prevents bugs caused when multiple card forms are placed on the same page.

---
+++ src/js/card-js.jquery.js
@@ -0,0 +1,37 @@
+(function($) {
+
+  var methods = {
+    init: function() {
+      new CardJs(this);
+    }
+  };
+
+
+  /**
+   * jQuery function.
+   *
+   * @param methodOrOptions
+   * @returns {*}
+   */
+  $.fn.CardJs = function(methodOrOptions) {
+    if(methods[methodOrOptions]) {
+      return methods[methodOrOptions].apply(this, Array.prototype.slice.call(arguments, 1));
+    } else if(typeof methodOrOptions === "object" || !methodOrOptions) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error("Method " +  methodOrOptions + " does not exist on jQuery.CardJs");
+    }
+  };
+
+
+}(jQuery));
+
+
+//
+// Initialise for all elements with cardjs class.
+//
+$(function() {
+  $(".cardjs").each(function(i, obj) {
+    $(obj).CardJs();
+  });
+});
